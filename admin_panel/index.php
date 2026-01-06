30083bfcc8e8f7201f0a11b104563a9a7e45b8b5
qunoot zehra <qunootzehra21@gmail.com>
2026-01-07 02:40:06 +0500

    chnages

---
+++ admin_panel/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin panel</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="admin-logo-img">
+            <img src="../pictures/logo1.png" alt="">
+        </div>
+        <div class="nav-message">
+            <h4>Welcome Qunoot</h4>
+            <button>Logout</button>
+        </div>
+    </nav>
+    <section>
+        <div class="admin-panel">
+            <div class="admin-profile">
+            <h1>Manage Details</h1>
+            
+                <img src="../pictures/profile.webp" alt="">
+            </div>
+            <div class="admin-btns">
+                <button class="admin-btn"><a href="">Insert Product</a></button>
+                <button class="admin-btn"><a href="">Veiw Product</a></button>
+                <button class="admin-btn"><a href="">Insert Category</a></button>
+                <button class="admin-btn"><a href="">Veiw Category</a></button>
+                <button class="admin-btn"><a href="">Insert Brand</a></button>
+                <button class="admin-btn"><a href="">Veiw Brand</a></button>
+                <button class="admin-btn"><a href="">All Payments</a></button>
+                <button class="admin-btn"><a href="">All Orders</a></button>
+                <button class="admin-btn"><a href="">List of Users</a></button>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
