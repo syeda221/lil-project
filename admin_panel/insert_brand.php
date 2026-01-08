799a7e5fddf6a3f4a745aad4a32edbcb91e8174b
qunoot zehra <qunootzehra21@gmail.com>
2026-01-08 19:33:18 +0500

    changes

--- admin_panel/insert_brand.php
+++ admin_panel/insert_brand.php
@@ -3,19 +3,25 @@
     include('../includes/connects.php') ;
     if(isset($_POST['insert_brand'])){
         $insert_brand = $_POST['brand_name'];
-    }
-    $sql = "insert into brands (brand_name) values('$insert_brand')";
-    $retval = mysqli_query($conn , $sql);
+        $select_row = "select * from brands where brand_name = '$insert_brand'";
+        $result_select = mysqli_query($conn , $select_row);
+        if(mysqli_num_rows($result_select)>0){
+        echo "<script>alert('brand is already inserted' )</script>";
+        }else{
+         $insert_row = "insert into brands (brand_name) values('$insert_brand')";
+    $retval = mysqli_query($conn , $insert_row);
     if($retval){
-        echo "<script>alert('brand is inserted )<?/script>";
+        echo "<script>alert('brand is inserted' )</script>";
     }else{
         die("could not insert brand".mysqli_error($conn));
     }
+    }
+}
     
     ?>
     
     
-    <form action="" method="post" class="insert-category">
+    <form action="" method="post" class="insert-form">
         <label for="form_control"></label>
         <span>:)</span>
         <input type="text" name="brand_name"  placeholder="username" id="">
--- admin_panel/insert_category.php
+++ admin_panel/insert_category.php
@@ -2,7 +2,7 @@
 
 
     <?php  include('../includes/connects.php') ;?>
-    <form action="" method="post" class="insert-category">
+    <form action="" method="post" class="insert-form">
         <label for="form_control"></label>
         <span>:)</span>
         <input type="text" name="form_control"  placeholder="username" id="">
