e4aeb255d9e1240cec7e0ae11143888d851a3d53
qunoot zehra <qunootzehra21@gmail.com>
2026-01-08 19:06:29 +0500

    updated

--- admin_panel/insert_brand.php
+++ admin_panel/insert_brand.php
@@ -1,11 +1,24 @@
 
-    <?php  include('../includes/connects.php') ;?>
+    <?php  
+    include('../includes/connects.php') ;
+    if(isset($_POST['insert_brand'])){
+        $insert_brand = $_POST['brand_name'];
+    }
+    $sql = "insert into brands (brand_name) values('$insert_brand')";
+    $retval = mysqli_query($conn , $sql);
+    if($retval){
+        echo "<script>alert('brand is inserted )<?/script>";
+    }else{
+        die("could not insert brand".mysqli_error($conn));
+    }
+    
+    ?>
     
     
     <form action="" method="post" class="insert-category">
         <label for="form_control"></label>
         <span>:)</span>
-        <input type="text" name="form_control"  placeholder="username" id="">
+        <input type="text" name="brand_name"  placeholder="username" id="">
         <input type="submit" value="Insert Brand" name="insert_brand">
     </form>
   
