9f95ecf0dddfcb70334d704cfa0a267262ba548d
qunoot zehra <qunootzehra21@gmail.com>
2026-01-09 01:10:10 +0500

    some changes

---
+++ admin_panel/insert_product.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert products</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <form action="" method="post" class="insert-form"><br>
+        <label for="product_title">Product Title</label><br>
+        <input type="text" name="product_title" placeholder="Product Title"><br>  
+        <label for="product-des">Product discription</label><br>
+        <input type="text" name="product-des"><br>
+        <label for="keyword">Product Keyword</label><br>
+        <input type="text" name="keyword">
+
+    </form>
+</body>
+</html>
