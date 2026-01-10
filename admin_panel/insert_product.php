a9bdfb7716f0fe7fd2cc3881588a938927d640ec
qunoot zehra <qunootzehra21@gmail.com>
2026-01-10 18:00:59 +0500

    update

--- admin_panel/insert_product.php
+++ admin_panel/insert_product.php
@@ -60,18 +60,62 @@ if(isset($_POST['add_product'])){
     $product_keyword = $_POST['keyword'];
     $product_cat = $_POST['select_category'];
     $product_brand = $_POST['select_brand'];
-    $img1 = $_FILES['product_image1'];
-    $img2 = $_FILES['product_image2'];
-    $img3 = $_FILES['product_image3'];
+    //giving name and temp name  =====STEP 1
+    $img1 = $_FILES['product_image1']['name'];
+    $img1_temp = $_FILES['product_image1']['tmp_name'];
+    $img2 = $_FILES['product_image2']['name'];
+    $img2_temp = $_FILES['product_image2']['tmp_name'];
+    $img3 = $_FILES['product_image3']['name'];
+    $img3_temp = $_FILES['product_image3']['tmp_name'];
     $price = $_POST['product_price'];
-    $add_query = "insert into  `product` (`product_title`,`product_discription` , `product_keyword` ,`cat_id` , `brand_id`, `product_image1`,`product_image2` ,`product_image3` , `product_price`) 
-    values('$product_title' ,'$product_dis','$product_keyword', '$product_cat','$product_brand','$img1','$img2' ,'$img3' ,'$price') ";
-    $add_result = mysqli_query($conn , $add_query);
-    if(!$add_result){
-        echo  "this is an error in your code " . mysqli_query_error();
-    }else{
-        echo "<script> alert('product is  inserted');</script>";
+
+    //folder address  =====STEP 2
+
+    $image1_folder = "../pictures/".$img1;
+    $image2_folder = "../pictures/".$img2;
+    $image3_folder = "../pictures/".$img3;
+
+   
+    //breaking image into extension  =====STEP 4
+
+    $file1_sep = explode("." , $img1);
+    $file2_sep = explode("." , $img2);
+    $file3_sep = explode("." , $img3);
+    
+    //geting extension  =====STEP 5
+
+    $file1_extension = end($file1_sep);
+    $file2_extension = end($file2_sep);
+    $file3_extension = end($file3_sep);
+
+    //allowed extensions =====STEP 6
+
+    $extension= ["jpeg","jpg", "png"]  ; 
+
+    //file exytension array  =====STEP 7
+    
+    $file_extension=[$file1_extension,$file2_extension,$file3_extension];
+
+    //checking extensions =====STEP 7
+    foreach($file_extension as $ext){
+    if(in_array($ext ,$extension) ){
+         //adding images to folder  =====STEP 3
+
+        move_uploaded_file($img1_temp,$image1_folder );
+        move_uploaded_file($img2_temp,$image2_folder );
+        move_uploaded_file($img3_temp,$image3_folder );
+
+          $add_query = "insert into  `product` (`product_title`,`product_discription` , `product_keyword` ,`cat_id` , `brand_id`, `product_image1`,`product_image2` ,`product_image3` , `product_price`) 
+        values('$product_title' ,'$product_dis','$product_keyword', '$product_cat','$product_brand','$img1','$img2' ,'$img3' ,'$price') ";
+            $add_result = mysqli_query($conn , $add_query);
+        if(!$add_result){
+            echo  "this is an error in your code " . mysqli_query_error();
+        }else{
+            echo "<script> alert('product is  inserted');</script>";
+        }
     }
+    }
+  
 }
 
 ?>
